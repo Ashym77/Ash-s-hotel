d1bc2f2f80ef7600e0261d482b77fb1f7f898ae9
Ashym77 <112933715+Ashym77@users.noreply.github.com>
2023-01-07 03:21:52 +0100

    testing buttons

--- contacts.js
+++ contacts.js
@@ -54,3 +54,16 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function refreshPage(){
+
+  if(confirm("Are you sure u want to refresh page"))
+  
+  {
+  
+    location.reload();
+  
+  }
+
+
+}
